1ed6eeba55cf20c2feb9f1e75a4de8bbcdca40f2
Daniel Lamando <me@danopia.net>
2012-08-30 12:42:23 +0200

    Start over with some nice, simple Sinatra.

---
+++ Gemfile
@@ -0,0 +1,14 @@
+ruby '1.9.3'
+source 'https://rubygems.org'
+
+# Web framework
+gem 'sinatra'
+gem 'haml'
+
+# HTTP server
+gem 'thin'
+
+# Database
+gem 'sequel'
+gem 'mysql2', :group => :production
+gem 'sqlite3', :groups => [:development, :test]
---
+++ app.rb
@@ -0,0 +1,46 @@
+require 'sinatra'
+require 'sequel'
+require 'logger'
+
+DB = if ENV.key? 'DATABASE_URL'
+  Sequel.connect ENV['DATABASE_URL']
+else
+  Sequel.sqlite
+end
+DB.loggers << Logger.new(STDOUT)
+
+unless DB.table_exists? :items
+  DB.create_table :items do
+    primary_key :id
+
+    Fixnum :user_id, :null => false
+    index :user_id
+    String :title, :null => false
+
+    DateTime :created_at, :null => false
+    index :created_at
+    DateTime :updated_at
+    DateTime :completed_at
+  end
+
+  [
+    'Make a pretty to-do template',
+    'Apply the template to the other apps too',
+    'Make to-dos actually work',
+    'Party hard',
+  ].each do |seed|
+    DB[:items] << {:user_id => 0, :title => seed, :created_at => Time.now}
+  end
+end
+
+get '/' do
+  @items = DB[:items].all
+  haml :index, :format => :html5
+end
+
+post '/' do
+  return 'Forgot to send something' unless params[:title]
+  
+  DB[:items] << {:user_id => 0, :title => params[:title], :created_at => Time.now}
+  redirect '/'
+end
